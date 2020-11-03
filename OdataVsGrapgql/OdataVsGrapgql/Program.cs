68ec61deebc87007c56a0b981e8d0a2825779d7c
olegzlod <olegzlodkov@gmail.com>
2020-11-03 19:19:19 +0200

    add

---
+++ OdataVsGrapgql/OdataVsGrapgql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataVsGrapgql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
